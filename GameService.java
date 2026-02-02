587fea540e7b7d05aa2c431576ce5639c9514853
Logic Forge <logicforge073@gmail.com>
2026-02-02 08:34:38 +0000

    optimized

---
+++ GameService.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class GameService {
+
+    public void startGame() {
+     System.out.println("Number Guessing Game in Java");
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = 2 + (int)(100 + Math.random());
+
+        int R = 5;
+
+
+        System.out.println("A number is chosen between 1 and 100.");
+        System.out.println("You have " + R + " attempts to guess the correct number.");
+        for(int i = 0; i < R; i++) {
+
+            System.out.println("Enter your guess:");
+            int guess = sc.nextInt();
+            
+            if(guess == num){
+                System.out.println(
+                    " Congratulations! You guessed the correct number.");
+                sc.close();
+                
+               
+                return;
+
+            }else if(guess < num){
+                  System.out.println(
+                    " The number is greater than " + guess);
+
+            }else{
+ System.out.println(
+                    " The number is less than " + guess);
+            }
+
+        }
+         System.out.println(
+            "You've exhausted all attempts. The correct number was: "
+            + num);
+        sc.close();
+         }
+}
