37fa07c3fc6c995a1f6f60ac4aac698dded5228e
Logic Forge <logicforge073@gmail.com>
2026-02-03 03:41:58 +0000

    search alogrithm

--- Main.java
+++ Main.java
@@ -2,11 +2,33 @@
 public class Main{
      public static void main(String args[]){
         
-       GameService  game  = new GameService();
+    //    GameService  game  = new GameService();
       // game.startGame();
 
-     Puzzle.playGame();
+      //Puzzle.playGame();
+
+      int arr[] = {2, 4, 5, 7, 8, 9, 12, 15};
+        int target = 15;
+        
+    int linearResult = SearchAlgorithms.linearSearch(arr, target);
+    int binaryResult = SearchAlgorithms.binarySearch(arr, target);
+    
+    if (linearResult != -1) {
+        System.out.println("Linear Search: Element Found At Index: " + linearResult);
+    } else {
+        System.out.println("Linear Search: Element Not Found");
+    }
+    if (binaryResult != -1) {
+        System.out.println("Binary Search: Element Found At Index: " + binaryResult);
+    } else {
+        System.out.println("Binary Search: Element Not Found");
+    }
+
+    
+
 
 
     }
+
+    
 }
